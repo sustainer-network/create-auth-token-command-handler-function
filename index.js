43524070b471e013ca1bf8e77cac91ebb95f0a55
Joao <ritterjv@gmail.com>
2019-08-23 18:38:35 -0700

    -

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const commandHandler = require("@sustainer-network/create-auth-token-command-handler");
 const tokensFromReq = require("@sustainer-network/tokens-from-req");
 const eventStore = require("@sustainer-network/event-store-js");
-const eventBus = require("@sustainer-network/event-bus");
 
 exports.command = (req, res) => {
   commandHandler({
-    body: req.body,
+    params: req.body,
     tokens: tokensFromReq(req),
-    publishEventFn: event => {
-      eventStore.add(event);
-      eventBus.publish(event);
-    }
+    publishEventFn: eventStore.add
   })
     .then(response => res.send(response))
     .catch(e => res.status(e.statusCode).send(e));

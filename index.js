95d6090872cdb50e9ec94573959c20a880ac15eb
Joao <ritterjv@gmail.com>
2019-08-24 18:11:11 -0700

    -

--- index.js
+++ index.js
@@ -9,5 +9,9 @@ exports.command = (req, res) => {
     publishEventFn: eventStore.add
   })
     .then(response => res.send(response))
-    .catch(e => res.status(e.statusCode).send(e));
+    .catch(e => {
+      //eslint-disable-next-line no-console
+      console.log("err: ", { e, stack: e.stack });
+      res.status(e.statusCode).send(e);
+    });
 };

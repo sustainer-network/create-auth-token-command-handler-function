9808d269c75c39c6d8f605bb492674937d8a5ed8
Joao <ritterjv@gmail.com>
2019-08-26 15:26:33 -0700

    -

--- test.js
+++ test.js
@@ -1,12 +1,33 @@
 const { expect } = require("chai");
 const { post } = require("@sustainer-network/request");
 const { fineTimestamp } = require("@sustainer-network/datetime");
+const { create } = require("@sustainer-network/jwt");
 
 const address =
   "https://create.auth-token.core.staging.sustainer.network/v1/issue";
 
 describe("Create auth token command", () => {
   it("should return successfully", async () => {
+    const principle = "some-principle-root";
+    const token = await create({
+      options: {
+        issuer: "create.auth-token.core.staging.sustainer.network",
+        subject: principle,
+        audience: ["*"],
+        expiresIn: 234234234234
+      },
+      data: {
+        root: "some-rooot",
+        principle,
+        scopes: ["*:*:*"],
+        context: {
+          network: "sustainer.network",
+          service: "core",
+          org: "some-org"
+        }
+      },
+      secret: process.env.SECRET
+    });
     const response = await post(
       address,
       {
@@ -19,7 +40,7 @@ describe("Create auth token command", () => {
           ip: "asdf"
         }
       },
-      { authorization: "" }
+      { authorization: `Bearer ${token}` }
     );
 
     expect(response.statusCode).to.equal(200);

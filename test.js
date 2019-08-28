3553819ac28d30658f0b8649111d3a9afaf7eed9
Joao <ritterjv@gmail.com>
2019-08-27 17:41:07 -0700

    -

--- test.js
+++ test.js
@@ -1,33 +1,33 @@
 const { expect } = require("chai");
 const { post } = require("@sustainer-network/request");
 const { fineTimestamp } = require("@sustainer-network/datetime");
-const { create } = require("@sustainer-network/jwt");
+// const { create } = require("@sustainer-network/jwt");
 
 const address =
   "https://create.auth-token.core.staging.sustainer.network/v1/issue";
 
 describe("Create auth token command", () => {
   it("should return successfully", async () => {
-    const principle = "some-principle-root";
-    const token = await create({
-      options: {
-        issuer: "create.auth-token.core.staging.sustainer.network",
-        subject: principle,
-        audience: "https://create.auth-token.core.staging.sustainer.network",
-        expiresIn: 234234234234
-      },
-      data: {
-        root: "some-rooot",
-        principle,
-        scopes: ["*:*:*"],
-        context: {
-          network: "sustainer.network",
-          service: "core",
-          org: "some-org"
-        }
-      },
-      secret: process.env.SECRET
-    });
+    // const principle = "some-principle-root";
+    // const token = await create({
+    //   options: {
+    //     issuer: "create.auth-token.core.staging.sustainer.network",
+    //     subject: principle,
+    //     audience: "https://create.auth-token.core.staging.sustainer.network",
+    //     expiresIn: 234234234234
+    //   },
+    //   data: {
+    //     root: "some-rooot",
+    //     principle,
+    //     scopes: ["*:*:*"],
+    //     context: {
+    //       network: "sustainer.network",
+    //       service: "core",
+    //       org: "some-org"
+    //     }
+    //   },
+    //   secret: process.env.SECRET
+    // });
     const response = await post(
       address,
       {
@@ -40,7 +40,7 @@ describe("Create auth token command", () => {
           ip: "asdf"
         }
       },
-      { authorization: `Bearer ${token}` }
+      { authorization: "Bearer BOOHOOOHOOHO" }
     );
 
     expect(response.statusCode).to.equal(200);

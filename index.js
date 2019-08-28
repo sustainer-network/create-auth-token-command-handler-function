fdb6c1ed1bae13d3857cc34a4d40b6fec5a4fb75
Joao <ritterjv@gmail.com>
2019-08-28 16:44:59 -0700

    Aborting CF and trying CR only

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+const express = require("express");
+const app = express();
+
 const commandHandler = require("@sustainer-network/create-auth-token-command-handler");
 const tokensFromReq = require("@sustainer-network/tokens-from-req");
 const eventStore = require("@sustainer-network/event-store-js");
 const logger = require("@sustainer-network/logger");
 
-exports.command = (req, res) => {
+app.post("/", (req, res) => {
   logger.info("REQ BE: ", { req });
   commandHandler({
     params: req.body,
@@ -15,4 +18,7 @@ exports.command = (req, res) => {
       logger.info("EEE BE: ", { req });
       res.status(e.statusCode).send(e);
     });
-};
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port);

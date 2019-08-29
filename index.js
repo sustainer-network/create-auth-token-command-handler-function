da6b3e3d545c212fb49dd5f92ef44b2bd1b8e75e
Joao <ritterjv@gmail.com>
2019-08-29 14:29:34 -0700

    -

--- index.js
+++ index.js
@@ -6,25 +6,15 @@ app.use(bodyParser.json());
 const commandHandler = require("@sustainer-network/create-auth-token-command-handler");
 const tokensFromReq = require("@sustainer-network/tokens-from-req");
 const eventStore = require("@sustainer-network/event-store-js");
-const logger = require("@sustainer-network/logger");
 
 app.post("/", (req, res) => {
-  logger.info("TARGET: ", { target: process.env.TARGET });
-  logger.info("REQ BE: ", { req });
-  logger.info("HEADERS BE: ", { headers: req.headers });
-  logger.info("BODY BE: ", { body: req.body });
-  logger.info("PARAMS BE: ", { body: req.params });
-  logger.info("QUERY BE: ", { body: req.query });
   commandHandler({
     params: req.body,
     tokens: tokensFromReq(req),
     publishEventFn: eventStore.add
   })
     .then(response => res.send(response))
-    .catch(e => {
-      logger.info("EEE BE: ", { e });
-      res.status(e.statusCode).send(e);
-    });
+    .catch(e => res.status(e.statusCode).send(e));
 });
 
 const port = process.env.PORT || 8080;
--- test.js
+++ test.js
@@ -1,32 +1,33 @@
 const { expect } = require("chai");
 const { post } = require("@sustainer-network/request");
 const { fineTimestamp } = require("@sustainer-network/datetime");
-// const { create } = require("@sustainer-network/jwt");
+const { create } = require("@sustainer-network/jwt");
 
 const url = "https://create.auth-token.core.staging.sustainer.network";
 
 describe("Create auth token command", () => {
   it("should return successfully", async () => {
-    // const principle = "some-principle-root";
-    // const token = await create({
-    //   options: {
-    //     issuer: "create.auth-token.core.staging.sustainer.network",
-    //     subject: principle,
-    //     audience: "https://create.auth-token.core.staging.sustainer.network",
-    //     expiresIn: 234234234234
-    //   },
-    //   data: {
-    //     root: "some-rooot",
-    //     principle,
-    //     scopes: ["*:*:*"],
-    //     context: {
-    //       network: "sustainer.network",
-    //       service: "core",
-    //       org: "some-org"
-    //     }
-    //   },
-    //   secret: process.env.SECRET
-    // });
+    const principle = "some-principle-root";
+    const token = await create({
+      options: {
+        issuer: "create.auth-token.core.staging.sustainer.network",
+        subject: principle,
+        audience: "https://create.auth-token.core.staging.sustainer.network",
+        expiresIn: 234234234234
+      },
+      data: {
+        root: "some-rooot",
+        principle,
+        scopes: ["*:*:*"],
+        context: {
+          network: "sustainer.network",
+          service: "core",
+          org: "some-org"
+        }
+      },
+      secret: process.env.SECRET
+    });
+
     const response = await post(
       url,
       {
@@ -37,11 +38,10 @@ describe("Create auth token command", () => {
         issuerInfo: {
           id: "asdf",
           ip: "asdf"
-        },
-        aNumber: 23
+        }
       },
       {
-        authorization: "Bearer BOOHOOOHOOHO"
+        authorization: token
       }
     );
 
@@ -51,8 +51,8 @@ describe("Create auth token command", () => {
 
     expect(JSON.parse(response.body).token).to.exist;
   });
-  // it("should return an error if incorrect params", async () => {
-  //   const response = await post(address, {});
-  //   expect(response.statusCode).to.be.at.least(400);
-  // });
+  it("should return an error if incorrect params", async () => {
+    const response = await post(url, {});
+    expect(response.statusCode).to.be.at.least(400);
+  });
 });

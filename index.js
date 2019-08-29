5f767a4ff4adfcc47e418960a779f7d961fd9d71
Joao <ritterjv@gmail.com>
2019-08-29 12:29:49 -0700

    -

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 const app = express();
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
 const commandHandler = require("@sustainer-network/create-auth-token-command-handler");
 const tokensFromReq = require("@sustainer-network/tokens-from-req");

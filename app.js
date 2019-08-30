588fb4fab420a9fba56e3f458994c15a1c42d2fe
Joao <ritterjv@gmail.com>
2019-08-30 16:53:12 -0700

    -

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ const commandHandler = require("@sustainer-network/create-auth-token-command-han
 const tokensFromReq = require("@sustainer-network/tokens-from-req");
 const eventStore = require("@sustainer-network/event-store-js");
 const { sign } = require("@sustainer-network/kms");
+const logger = require("@sustainer-network/logger");
 
 app.use(bodyParser.json());
 
 app.post("/", (req, res) => {
+  logger.info("env: ", process.env);
   commandHandler({
     params: req.body,
     tokens: tokensFromReq(req),
